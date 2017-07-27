0a84c654b9a3d534b619846628d10943756cb200
FreeFlight-Code <davidfisc@hotmail.com>
2017-07-26 21:40:02 -0600

    20% completed

--- js/js.js
+++ js/js.js
@@ -1,6 +1,20 @@
-console.log("ready")
+
 $(document).ready(function(){
-  $(".avatar").on("click", function(){
-    $(this).hide();
+  $(".tweet-compose").on("click", function(){
+    $(this).css("height", "5em");
+    $("#tweet-controls").css("display", "inline");
+    $("#tweet-controls").css("float", "right");
+  });//tweet compose close
+//  Edit Below This Line!!!!!!!!!!!!!!!!!!!!!!!!!
+  $(".tweet-compose").on("click", function(){
+    $(".stats").css("display", "inline-block");
   });
-});
+  //
+  $(".tweets").hover (function(){
+      $(".tweet-actions").css("display", "inline");
+   });
+});//This is the closure for my document ready jquery
+
+
+
+// Do Not Edit Above This Line!!!!!!!!!!!!!!!!!!!!!!!!!
